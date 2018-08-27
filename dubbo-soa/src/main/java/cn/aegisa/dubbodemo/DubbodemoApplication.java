3735be68a6dfbeb5d82005ec3c03398e50cb3973
缐英达 <daegis@yeah.net>
2018-08-27 16:27:40 +0800

    another dubbo implements

--- dubbo-soa/src/main/java/cn/aegisa/dubbodemo/DubbodemoApplication.java
+++ dubbo-soa/src/main/java/cn/aegisa/dubbodemo/DubbodemoApplication.java
@@ -1,11 +1,9 @@
 package cn.aegisa.dubbodemo;
 
-import com.alibaba.dubbo.spring.boot.annotation.EnableDubboConfiguration;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-@EnableDubboConfiguration
 public class DubbodemoApplication {
 
     public static void main(String[] args) {
